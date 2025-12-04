a8cd177cce0bc6f4eac46789d7450c47e639a7c8
TascaBarea <tascabarea@gmail.com>
2025-12-04 10:08:03 +0100

    feat: añadir validador de patrones

--- src/facturas/utils/__init__.py
+++ src/facturas/utils/__init__.py
@@ -3,5 +3,12 @@ Utilidades para ParsearFacturas
 """
 
 from .logger import crear_logger, log_factura, log_resumen
+from .validador import validar_patron, validar_carpeta
 
-__all__ = ['crear_logger', 'log_factura', 'log_resumen']
+__all__ = [
+    'crear_logger', 
+    'log_factura', 
+    'log_resumen',
+    'validar_patron',
+    'validar_carpeta',
+]
